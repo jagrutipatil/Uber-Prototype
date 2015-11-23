9457357e1340fb636d67ffafd9383b005458ed0f
jagrutipatil <jagrutipatil32@gmail.com>
2015-11-23 15:39:09 -0800

    Added more APIs to customer module

--- RabbitMQ-Backend-Uber/services/customer.js
+++ RabbitMQ-Backend-Uber/services/customer.js
@@ -2,9 +2,10 @@ var ejs = require("ejs");
 var mySqlDb = require("./mysqldb");
 var tableName = "testdb.customers";
 
-function signup(email, password, firstname, lastname, mobileno, cardno, cvv, exp_date, postalcode, callback) {
-	var sqlQuery = "INSERT INTO "+ tableName + " ( email, password, firstname, lastname, mobileno, cardno, cvv, ex_date, postalcode) VALUES ( '" + email 
-	+ "' , '" + password +  
+function signup(ssn, email, password, firstname, lastname, mobileno, cardno, cvv, exp_date, postalcode, callback) {
+	var sqlQuery = "INSERT INTO "+ tableName + " ( ssn, email, password, firstname, lastname, mobileno, cardno, cvv, ex_date, postalcode, approved) VALUES ( '" + email 
+	+ "' , '" + ssn +
+	  "' , '" + password +  
 	  "' , '" + firstname  +
 	  "' , '" + lastname +
 	  "' , '" + mobileno +
@@ -12,7 +13,7 @@ function signup(email, password, firstname, lastname, mobileno, cardno, cvv, exp
 	  "' , '" + cvv +
 	  "' , '" + exp_date +
 	  "' , '" + postalcode +
-			"' )";
+	  "' , ' false' )";
 	
 	mySqlDb.executeQuery(function(err, rows) {
 		if (!err) {
@@ -25,6 +26,91 @@ function signup(email, password, firstname, lastname, mobileno, cardno, cvv, exp
 		}
 		callback(res);
 	}, sqlQuery);
+}
+
+function approve(ssn, callback) {
+	var sqlQuery = "UPDATE "+ tableName + " SET approved = 'true' WHERE ssn = '" + ssn+"'";
+	  	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+	    	  res.code = "200";
+			  res.value = "Succes approve";
+		} else {
+			  console.log(err);
+			  res.code = "401";
+			  res.value = "Failed approve";
+		}
+		callback(res);
+	}, sqlQuery);
+}
+
+function update(ssn, email, password, firstname, lastname, mobileno, postalcode, callback) {
+	var sqlQuery = "UPDATE "+ tableName + " SET email = '"+ email 
+	+ "', password = '"+password+"' , firstname = '"+firstname+"' , lastname = '"+lastname+"', +" +
+			" mobileno = '"+mobileno+"', postalcode = '"+postalcode+"') WHERE ssn = '" + ssn+"'";
+	  	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+	    	  res.code = "200";
+			  res.value = "Succes update";
+		} else {
+			  console.log(err);
+			  res.code = "401";
+			  res.value = "Failed update";
+		}
+		callback(res);
+	}, sqlQuery);
+}
+
+function search_with_ssn(ssn) {
+	var sqlQuery = "SELECT * FROM " + tableName + " WHERE ssn='"+ ssn +"'";
+	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+			if (rows.length > 0) {
+				res.val = rows[0];
+				res.code = "200";
+			} else {
+				console.log(err);
+		        res.code = "401";
+				res.value = "Failed Login";				
+			}			
+		}
+	}, sqlQuery);	
+}
+
+function search_with_email(email) {
+	var sqlQuery = "SELECT * FROM " + tableName + " WHERE email='"+ email +"'";
+	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+			if (rows.length > 0) {
+				res.val = rows[0];
+				res.code = "200";
+			} else {
+				console.log(err);
+		        res.code = "401";
+				res.value = "Failed Login";				
+			}			
+		}
+	}, sqlQuery);	
+}
+
+function search_with_name(firstname, lastname) {
+	var sqlQuery = "SELECT * FROM " + tableName + " WHERE firstname='"+ firstname +"' AND lastname = '" + lastname + "'";
+	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+			if (rows.length > 0) {
+				res.val = rows[0];
+				res.code = "200";
+			} else {
+				console.log(err);
+		        res.code = "401";
+				res.value = "Failed Login";				
+			}			
+		}
+	}, sqlQuery);	
 }
 
 
@@ -45,7 +131,40 @@ function signin(email, password) {
 	}, sqlQuery);
 }
 
-function remove(email, password) {	
+
+function selectAll() {	
+	var sqlQuery = "SELECT * FROM " + tableName ;
+	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+			if (rows.length > 0) {
+				res.val = rows;
+				res.code = "200";
+			} else {
+				console.log(err);
+		        res.code = "401";
+				res.value = "Failed Login";				
+			}			
+		}
+	}, sqlQuery);
+}
+
+function remove_with_ssn(ssn) {	
+	var sqlQuery = "DELETE FROM " + tableName + " WHERE ssn='"+ ssn +"'";
+	
+	mySqlDb.executeQuery(function(err, rows) {
+		if (!err) {
+	    	  res.code = "200";
+			  res.value = "Succes Login";
+		} else {
+			  console.log(err);
+			  res.code = "401";
+			  res.value = "Failed Login";
+		}
+	}, sqlQuery);
+}
+
+function remove_with_email(email) {	
 	var sqlQuery = "DELETE FROM " + tableName + " WHERE email='"+ email +"' AND password='"+ password+"'";
 	
 	mySqlDb.executeQuery(function(err, rows) {
@@ -62,4 +181,11 @@ function remove(email, password) {
 
 exports.signup = signup;
 exports.signin = signin;
-exports.remove = remove;
+exports.remove_with_email = remove_with_email;
+exports.remove_with_ssn = remove_with_ssn;
+exports.selectAll = selectAll;
+exports.search_with_name = search_with_name;
+exports.search_with_email = search_with_email; 
+exports.search_with_ssn = search_with_ssn;
+exports.update = update;
+exports.approve = approve;
