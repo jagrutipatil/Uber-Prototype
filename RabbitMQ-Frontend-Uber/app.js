e53c1dcaf4f0dcd35e75fa002552661cdffe5ec9
jagrutipatil <jagrutipatil32@gmail.com>
2015-11-23 19:56:40 -0800

    added APIs

--- RabbitMQ-Frontend-Uber/app.js
+++ RabbitMQ-Frontend-Uber/app.js
@@ -5,7 +5,8 @@
 
 var express = require('express');
 var routes = require('./routes');
-var login = require('./routes/login');
+var customer = require('./routes/customerClient');
+var driver = require('./routes/driverClient');
 var http = require('http');
 var path = require('path');
 var amqp = require('amqp')
@@ -29,9 +30,27 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', login.home);
-app.post('/login', login.signin);
-app.post('/signup', login.signup);
+
+app.post('/customer_signin', customer.signin);
+app.post('/customer_signup', customer.signup);
+app.post('/customer_remove_with_email', customer.remove_with_email);
+app.post('/customer_remove_with_ssn', customer.remove_with_ssn);
+app.post('/customer_selectAll', customer.selectAll);
+app.post('/customer_search_with_name', customer.search_with_name);
+app.post('/customer_search_with_ssn', customer.search_with_ssn);
+app.post('/customer_update', customer.update);
+app.post('/customer_approve', customer.approve);
+
+app.post('/driver_signin', driver.signin);
+app.post('/driver_signup', driver.signup);
+app.post('/driver_remove_with_email', driver.remove_with_email);
+app.post('/driver_remove_with_ssn', driver.remove_with_ssn);
+app.post('/driver_selectAll', driver.selectAll);
+app.post('/driver_search_with_name', driver.search_with_name);
+app.post('/driver_search_with_ssn', driver.search_with_ssn);
+app.post('/driver_update', driver.update);
+app.post('/driver_approve', driver.approve);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
