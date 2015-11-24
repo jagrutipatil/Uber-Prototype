cbc00b0a4d1f5ba9cc2844d83da4659f4c7bd674
jagrutipatil <jagrutipatil32@gmail.com>
2015-11-23 16:43:08 -0800

    changing queue name

--- RabbitMQ-Backend-Uber/server.js
+++ RabbitMQ-Backend-Uber/server.js
@@ -15,7 +15,7 @@ var cnn = amqp.createConnection({host:'127.0.0.1'});
 cnn.on('ready', function(){
 	console.log("listening on login_queue");
 
-	cnn.queue('login_queue', function(q){
+	cnn.queue('customer', function(q){
 		q.subscribe(function(message, headers, deliveryInfo, m){
 			util.log(util.format( deliveryInfo.routingKey, message));
 			util.log("Message: "+JSON.stringify(message));
