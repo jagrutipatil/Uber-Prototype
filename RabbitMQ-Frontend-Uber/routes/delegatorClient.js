e2d4981b7d816739daeeeef3944a8b6fc1614126
jagrutipatil <jagrutipatil32@gmail.com>
2015-11-24 17:20:31 -0800

    Changing API names

---
+++ RabbitMQ-Frontend-Uber/routes/delegatorClient.js
@@ -0,0 +1,43 @@
+
+var ejs = require("ejs");
+var mq_client = require('./client');
+
+function loginCustomer(req, res) {
+	ejs.renderFile("./views/loginCustomer.ejs", function(err, result) {
+		if (!err) {
+			res.end(result);
+		}
+	});
+}
+
+
+function signupCustomer(req, res) {
+	ejs.renderFile("./views/SignupCustomer.ejs", function(err, result) {
+		if (!err) {
+			res.end(result);
+		}
+	});
+}
+
+
+function loginDriver(req, res) {
+	ejs.renderFile("./views/loginDriver.ejs", function(err, result) {
+		if (!err) {
+			res.end(result);
+		}
+	});
+}
+
+
+function signupDriver(req, res) {
+	ejs.renderFile("./views/SignupDriver.ejs", function(err, result) {
+		if (!err) {
+			res.end(result);
+		}
+	});
+}
+
+exports.loginCustomer = loginCustomer;
+exports.signupCustomer = signupCustomer;
+exports.loginDriver = loginDriver;
+exports.signupDriver = signupDriver;
