a6652316c8bb45810c48127b2aae9d26513f87f0
jagrutipatil <jagrutipatil32@gmail.com>
2015-11-26 03:18:40 -0800

    Adding controller for login driver

---
+++ RabbitMQ-Frontend-Uber/public/controllers/loginDriver.js
@@ -0,0 +1,23 @@
+var app = angular.module('loginDriver', []);
+app.controller("loginDriverController", loginDriverController);
+loginDriverController.$inject = [ '$scope', '$http', '$window'];
+function loginDriverController($scope, $http, $window) {
+	$scope.signin = function() {
+		$http({
+			method : 'POST',
+			url : '/bk_driver_signin',
+			data : {
+				"email" : $scope.email,
+				"password": $scope.password
+			}
+		}).success(function(response) {
+			if (response.result != "error") {
+				alert("Success");
+			} else {
+				alert("error");
+			}			
+		}).error(function(error) {
+			console.log(error);
+		});
+	};
+}
