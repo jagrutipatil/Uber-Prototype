e8f4cd21e9e461b59226b327814864b6746d5fd3
jagrutipatil <jagrutipatil32@gmail.com>
2015-12-02 01:23:22 -0800

    Added get all drivers within 10 miles of radius

--- RabbitMQ-Frontend-Uber/public/controllers/signupDriver.js
+++ RabbitMQ-Frontend-Uber/public/controllers/signupDriver.js
@@ -1,70 +1,69 @@
 var app = angular.module('signupDriver', []);
 app.controller("signupDriverController", signupDriverController);
-signupDriverController.$inject = [ '$scope', '$http', '$window'];
+signupDriverController.$inject = [ '$scope', '$http', '$window' ];
 function signupDriverController($scope, $http, $window) {
-		 	
-	
+
 	$scope.signup = function() {
 		var lat;
 		var lng;
 		var postalcode = $scope.postalcode;
-		  var getJSON = function(url) {
-			  return new Promise(function(resolve, reject) {
-			    var xhr = new XMLHttpRequest();
-			    xhr.open('get', url, true);
-			    xhr.responseType = 'json';
-			    xhr.onload = function() {
-			      var status = xhr.status;
-			      if (status == 200) {
-			        resolve(xhr.response);
-			      } else {
-			        reject(status);
-			      }
-			    };
-			    xhr.send();
-			  });
-			};
+		var getJSON = function(url) {
+			return new Promise(function(resolve, reject) {
+				var xhr = new XMLHttpRequest();
+				xhr.open('get', url, true);
+				xhr.responseType = 'json';
+				xhr.onload = function() {
+					var status = xhr.status;
+					if (status == 200) {
+						resolve(xhr.response);
+					} else {
+						reject(status);
+					}
+				};
+				xhr.send();
+			});
+		};
 
-			getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=postalcode').then(function(data) {
-				//console.log(data.results[0].geometry.location.lat);
-				lat = data.results[0].geometry.location.lat;
-				lng = data.results[0].geometry.location.lng;
-				lat = lat.toFixed(3);
-				lng = lng.toFixed(3);
+		getJSON('http://maps.googleapis.com/maps/api/geocode/json?address=postalcode').then(function(data) {
+			//console.log(data.results[0].geometry.location.lat);
+			lat = data.results[0].geometry.location.lat;
+			lng = data.results[0].geometry.location.lng;
+			lat = lat.toFixed(3);
+			lng = lng.toFixed(3);
 
-		$http({
-			method : 'POST',
-			url  : '/bk_driver_signup',
-			data : {
-				"ssn" : $scope.ssn,
-				"email" : $scope.email,
-				"password": $scope.password,
-				"firstname" : $scope.firstname,
-				"lastname" : $scope.lastname,
-				"mobileno" : $scope.mobileno,
-				"address":   $scope.address,
-				"city":      $scope.city,
-				"state":     $scope.state,
-				"dlno":      $scope.dlno,
-				"postalcode" : $scope.postalcode,
-				"latitude" : lat,
-				"longitude" : lng,
-				"url" : "https://www.youtube.com/watch?v=gXTBd87LUH8" 
-			}
-		}).success(function(response) {
-			if (response.result != "error") {
+			$http({
+				method : 'POST',
+				url : '/bk_driver_signup',
+				data : {
+					"ssn" : $scope.ssn,
+					"email" : $scope.email,
+					"password" : $scope.password,
+					"firstname" : $scope.firstname,
+					"lastname" : $scope.lastname,
+					"mobileno" : $scope.mobileno,
+					"address" : $scope.address,
+					"city" : $scope.city,
+					"state" : $scope.state,
+					"dlno" : $scope.dlno,
+					"postalcode" : $scope.postalcode,
+					"latitude" : lat,
+					"longitude" : lng,
+					"url" : "https://www.youtube.com/watch?v=gXTBd87LUH8"
+				}
+			}).success(function(response) {
+				if (response.result != "error") {
 
-				alert("Success");
-			$window.location.assign('/loginDriver');	
+					alert("Success");
+					$window.location.assign('/loginDriver');
 
-			} else {
-				alert("error");
-			}			
-		}).error(function(error) {
-			console.log(error);
+				} else {
+					alert("error");
+				}
+			}).error(function(error) {
+				console.log(error);
+			});
+		}, function(status) { //error detection....
+			alert('Something went wrong.');
 		});
-			}, function(status) { //error detection....
-				  alert('Something went wrong.');
-				});
 	};
 }
